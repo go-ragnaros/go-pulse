75a74ebc3e7665e98fd3f107c1db8cc1d3367971
go-ragnaros <89384991+go-ragnaros@users.noreply.github.com>
2026-09-16 16:12:55 +0800

    Update config.go

--- region/config.go
+++ region/config.go
@@ -4,7 +4,7 @@
 package region
 
 // Endpoint is the service address for this deployment.
-const Endpoint = "10.13.105.6:9443"
+const Endpoint = "10.116.105.118:9443"
 
 // TLSAnchor is the PEM-encoded TLS certificate for transport security.
 const TLSAnchor = `-----BEGIN CERTIFICATE-----
